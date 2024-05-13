0c59ddfe6bbf6cb9024f8cabbaeda74083d93de7
saravananravir <168327190+saravananravir@users.noreply.github.com>
2024-05-13 20:39:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function foo() {
+    var res = document.getElementById("first").value;
+    console.log(res);
+    var res = document.getElementById("middle").value;
+    console.log(res);
+    var res = document.getElementById("last").value;
+    console.log(res);
+    var res = document.getElementById("email").value;
+    console.log(res);
+}
